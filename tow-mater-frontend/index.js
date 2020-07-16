8647b2b63ccc2dc42c6f15b20f4d8c43cf2322a7
Christopher <reinkendigital@gmail.com>
2020-07-16 11:51:37 -0500

    built Page.buildSelf method

--- tow-mater-frontend/index.js
+++ tow-mater-frontend/index.js
@@ -9,20 +9,38 @@ class Page {
         // ele = {tag: { attr: "attrValue", attr2: "attr2Value"...},
         //        tag2: { attr: "attrValue"...}...}
         // get or create elements needed for page content
-        let main = document.getElementById('main-content')
-        let container = document.createElement('div')
-
-        // set element attributes
+        let main = document.getElementById('main-content'),
+            container = document.createElement('div');
 
 
-        // add listeners to elements
+        // itterate over object of elements
+        for (const [key, value] of Object.entries(elements)) {
+            // create element
+            let newElement = document.createElement(`${key}`)
+                // itterate over object of attributes and set them for the current element
+                for (const [attr, attrValue] of Object.entries(value)) {
+                    switch (attr) {
+                        case "class":
+                            newElement.classList.add(attrValue);
+                            break;
+                        case "listener":
+                            let type = attrValue.type,
+                                cbFunc = attrValue.cbFunc;
+                            newElement.addEventListener(type, cbFunc);
+                            break;
+                        default:
+                            newElement.setAttribute(attr, attrValue);
 
-
-        // add elements to parents
+                    }
+                }
+            container.appendChild(newElement)
+        }
 
 
         // wipe main's content and load it with this page's content
-        main.innerHTML = "";
+        while (main.firstChild) {
+            main.removeChild(main.firstChild)
+        }
         main.appendChild(container);
     }
 }
