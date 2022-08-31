ae5f8702b5d86e841beee928055f97c4a9ae8199
creinken <reinkendigital@gmail.com>
2022-08-31 05:44:55 -0500

    reconfigured App and index files

---
+++ tow-mater-frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Navigate, useNavigate, useLocation } from 'react-router-dom';
+
+import { history } from '_helpers';
+import { Nav, PrivateRoute } from '_components';
+import { Home } from 'home';
+import { Login } from 'login';
+import './App.css';
+
+function App() {
+  history.navigate = useNavigate();
+  history.location = useLocation();
+
+  return (
+    <div className='app-container bg-light'>
+      <Nav />
+      <div className='container pt-4 pb-4'>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
