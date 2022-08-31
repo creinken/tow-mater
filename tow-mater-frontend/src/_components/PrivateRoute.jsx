1c51fca621a7ae07feb2f1bdcbcece5fb37b5efc
creinken <reinkendigital@gmail.com>
2022-08-30 21:54:42 -0500

    added initial front end config + helpers and comps

---
+++ tow-mater-frontend/src/_components/PrivateRoute.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { history } from '_helpers';
+
+function PrivateRoute({ children }) {
+    const { user: authUser } = useSelector(x => x.auth);
+
+    // if not logged in redirect to login page with a return url
+    if (!authUser) {
+        return <Navigate to='/login' state={{ from: history.location }} />
+    }
+
+    // authorize so return child components
+    return children;
+}
+
+export default PrivateRoute
